2ea939f0b96e8174dfaa330db55b0964e8d713a2
Damian Tomczyszyn <damitom@o2.pl>
2023-03-12 11:35:19 +0100

    meaby working

--- src/main/java/com/damiantomczyszyn/bookstore/security/SecurityConfiguration.java
+++ src/main/java/com/damiantomczyszyn/bookstore/security/SecurityConfiguration.java
@@ -3,23 +3,40 @@ package com.damiantomczyszyn.bookstore.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-//https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
-@Configuration
-public class SecurityConfiguration {
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests((authz) -> authz
-//                        .anyRequest().authenticated()
-//                )
-//                .httpBasic(withDefaults());
-//        return http.build();
-//    }
+    @Configuration
+    @EnableWebSecurity
+    public class SecurityConfiguration {
 
 
-}
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeHttpRequests((requests) -> requests
+                            .requestMatchers("/vertretungsplan").hasAnyRole("SCHUELER", "LEHRER", "VERWALTUNG")
+                            .anyRequest().authenticated()
+                    )
+                    .formLogin((form) -> form
+                            .loginPage("/login")
+                            .permitAll()
+                    )
+                    .logout((logout) -> logout.permitAll());
+
+            return http.build();
+        }
+
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+            return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+        }
+
+    }
