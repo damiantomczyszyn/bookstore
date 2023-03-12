a87c1899cfb3f7a636193ec084638ac907cc3f5b
Damian Tomczyszyn <damitom@o2.pl>
2023-03-12 11:44:58 +0100

    really working spring security

--- src/main/java/com/damiantomczyszyn/bookstore/security/SecurityConfiguration.java
+++ src/main/java/com/damiantomczyszyn/bookstore/security/SecurityConfiguration.java
@@ -2,10 +2,14 @@ package com.damiantomczyszyn.bookstore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
     @Configuration
@@ -17,23 +21,33 @@ import org.springframework.security.web.SecurityFilterChain;
         public BCryptPasswordEncoder passwordEncoder() {
             return new BCryptPasswordEncoder();
         }
-
         @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .authorizeHttpRequests((requests) -> requests
-                            .requestMatchers("/vertretungsplan").hasAnyRole("SCHUELER", "LEHRER", "VERWALTUNG")
-                            .anyRequest().authenticated()
-                    )
-                    .formLogin((form) -> form
-                            .loginPage("/login")
-                            .permitAll()
-                    )
-                    .logout((logout) -> logout.permitAll());
-
-            return http.build();
+        public InMemoryUserDetailsManager userDetailsService() {
+            UserDetails user1 = User.withUsername("user1")
+                    .password(passwordEncoder().encode("user1Pass"))
+                    .roles("USER")
+                    .build();
+            UserDetails user2 = User.withUsername("user2")
+                    .password(passwordEncoder().encode("user2Pass"))
+                    .roles("USER")
+                    .build();
+            UserDetails admin = User.withUsername("admin")
+                    .password(passwordEncoder().encode("adminPass"))
+                    .roles("ADMIN")
+                    .build();
+            return new InMemoryUserDetailsManager(user1, user2, admin);
         }
-
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+            return httpSecurity
+                    .authorizeHttpRequests(requests -> {
+                        requests.requestMatchers("/v1/home").hasRole("ADMIN");
+                        requests.requestMatchers("/v2/home").hasRole("USER");
+                        requests.anyRequest().denyAll();
+                    })
+                    .httpBasic(Customizer.withDefaults())
+                    .build();
+        }
         @Bean
         public WebSecurityCustomizer webSecurityCustomizer() {
             return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
