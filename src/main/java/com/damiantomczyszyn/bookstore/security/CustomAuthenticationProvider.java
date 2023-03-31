f3e69baeaa164a5e7f97dc8c1f3567a73056587e
Damian Tomczyszyn <damitom@o2.pl>
2023-03-31 13:34:41 +0200

    wyrzucanie wyjatku zamiast nulla

--- src/main/java/com/damiantomczyszyn/bookstore/security/CustomAuthenticationProvider.java
+++ src/main/java/com/damiantomczyszyn/bookstore/security/CustomAuthenticationProvider.java
@@ -3,6 +3,7 @@ package com.damiantomczyszyn.bookstore.security;
 import com.damiantomczyszyn.bookstore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -11,9 +12,12 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 
 @Component
-public class CustomAuthenticatorProvider implements AuthenticationProvider {
+public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     UserRepository repository;
+    public CustomAuthenticationProvider() {
+        super();
+    }
 
 
     @Override
@@ -22,7 +26,10 @@ public class CustomAuthenticatorProvider implements AuthenticationProvider {
 
         String name = authentication.getName();
         String password = authentication.getCredentials().toString();
+        System.out.println(name);
+        System.out.println(password);
         if (shouldAuthenticateAgainstThirdPartySystem(name, password)) {
+
 
 
 
@@ -31,11 +38,14 @@ public class CustomAuthenticatorProvider implements AuthenticationProvider {
             return new UsernamePasswordAuthenticationToken(
                     name, password, new ArrayList<>());
         } else {
-            return null;
+            throw  new BadCredentialsException("nie poprawne logowanie");
         }
     }
 
     private boolean shouldAuthenticateAgainstThirdPartySystem(String name, String password) {
+
+        //wyswietle całą kolekcje
+
         var userOptional = repository.findByEmail(name);
 
         if(userOptional.isPresent()){
--- src/main/java/com/damiantomczyszyn/bookstore/security/SecurityConfiguration.java
+++ src/main/java/com/damiantomczyszyn/bookstore/security/SecurityConfiguration.java
@@ -34,7 +34,7 @@ import org.springframework.security.web.SecurityFilterChain;
         }
 
         @Autowired
-        private CustomAuthenticatorProvider authProvider;
+        private CustomAuthenticationProvider authProvider;
 
 
 
